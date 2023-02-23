7a5e39f2c4af1bbb3b7c6c26b865f8ee53eb4a5b
liuguoqiang <janstilau@gmail.com>
2023-02-23 15:17:48 +0800

    sync

--- Example/PromiseExample/PromiseExample/ViewController.swift
+++ Example/PromiseExample/PromiseExample/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
 }
 
